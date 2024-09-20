e75f85af7d034035a3eb1d87f4e8367ad798d33f
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2024-09-20 15:21:15 +0200

    Creation du diagram de classe

---
+++ src/Entity/FicheFrais.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FicheFraisRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FicheFraisRepository::class)]
+class FicheFrais
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $mois = null;
+
+    #[ORM\Column]
+    private ?int $nbJustificatifs = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $montantValid = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateModif = null;
+
+    #[ORM\ManyToOne(inversedBy: 'ficheFrais')]
+    private ?User $User = null;
+
+    #[ORM\ManyToOne(inversedBy: 'ficheFrais')]
+    private ?Etat $Etat = null;
+
+    /**
+     * @var Collection<int, LigneFraisForfait>
+     */
+    #[ORM\OneToMany(targetEntity: LigneFraisForfait::class, mappedBy: 'ficheFrais')]
+    private Collection $LigneFraisForfait;
+
+    /**
+     * @var Collection<int, LigneFraisHorsForfait>
+     */
+    #[ORM\OneToMany(targetEntity: LigneFraisHorsForfait::class, mappedBy: 'ficheFrais')]
+    private Collection $LigneFraisHorsForfait;
+
+    public function __construct()
+    {
+        $this->LigneFraisForfait = new ArrayCollection();
+        $this->LigneFraisHorsForfait = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getMois(): ?\DateTimeInterface
+    {
+        return $this->mois;
+    }
+
+    public function setMois(\DateTimeInterface $mois): static
+    {
+        $this->mois = $mois;
+
+        return $this;
+    }
+
+    public function getNbJustificatifs(): ?int
+    {
+        return $this->nbJustificatifs;
+    }
+
+    public function setNbJustificatifs(int $nbJustificatifs): static
+    {
+        $this->nbJustificatifs = $nbJustificatifs;
+
+        return $this;
+    }
+
+    public function getMontantValid(): ?string
+    {
+        return $this->montantValid;
+    }
+
+    public function setMontantValid(string $montantValid): static
+    {
+        $this->montantValid = $montantValid;
+
+        return $this;
+    }
+
+    public function getDateModif(): ?\DateTimeInterface
+    {
+        return $this->dateModif;
+    }
+
+    public function setDateModif(\DateTimeInterface $dateModif): static
+    {
+        $this->dateModif = $dateModif;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): static
+    {
+        $this->User = $User;
+
+        return $this;
+    }
+
+    public function getEtat(): ?Etat
+    {
+        return $this->Etat;
+    }
+
+    public function setEtat(?Etat $Etat): static
+    {
+        $this->Etat = $Etat;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, LigneFraisForfait>
+     */
+    public function getLigneFraisForfait(): Collection
+    {
+        return $this->LigneFraisForfait;
+    }
+
+    public function addLigneFraisForfait(LigneFraisForfait $ligneFraisForfait): static
+    {
+        if (!$this->LigneFraisForfait->contains($ligneFraisForfait)) {
+            $this->LigneFraisForfait->add($ligneFraisForfait);
+            $ligneFraisForfait->setFicheFrais($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLigneFraisForfait(LigneFraisForfait $ligneFraisForfait): static
+    {
+        if ($this->LigneFraisForfait->removeElement($ligneFraisForfait)) {
+            // set the owning side to null (unless already changed)
+            if ($ligneFraisForfait->getFicheFrais() === $this) {
+                $ligneFraisForfait->setFicheFrais(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, LigneFraisHorsForfait>
+     */
+    public function getLigneFraisHorsForfait(): Collection
+    {
+        return $this->LigneFraisHorsForfait;
+    }
+
+    public function addLigneFraisHorsForfait(LigneFraisHorsForfait $ligneFraisHorsForfait): static
+    {
+        if (!$this->LigneFraisHorsForfait->contains($ligneFraisHorsForfait)) {
+            $this->LigneFraisHorsForfait->add($ligneFraisHorsForfait);
+            $ligneFraisHorsForfait->setFicheFrais($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLigneFraisHorsForfait(LigneFraisHorsForfait $ligneFraisHorsForfait): static
+    {
+        if ($this->LigneFraisHorsForfait->removeElement($ligneFraisHorsForfait)) {
+            // set the owning side to null (unless already changed)
+            if ($ligneFraisHorsForfait->getFicheFrais() === $this) {
+                $ligneFraisHorsForfait->setFicheFrais(null);
+            }
+        }
+
+        return $this;
+    }
+}
