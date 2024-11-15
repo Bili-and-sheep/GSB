1171caffe0c0a5b2fdf38a7addd77dca89574da6
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2024-11-15 15:38:59 +0100

    Carabistouille en cours

---
+++ src/Controller/SelectFicheController.php
@@ -0,0 +1,34 @@
+<?php
+
+// src/Controller/SelectFicheController.php
+
+namespace App\Controller;
+
+use App\Form\SelectFicheType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class SelectFicheController extends AbstractController
+{
+    #[Route('/selectfiche', name: 'app_select_fiche')]
+    public function index(Request $request,UserInterface $user): Response
+    {
+        $form = $this->createForm(SelectFicheType::class, null, ['user' => $user]);
+        $form->handleRequest($request);
+
+        $selectedFiche = null;
+        if ($form->isSubmitted() && $form->isValid()) {
+            $selectedFiche = $form->get('fiche')->getData();
+        }
+
+            return $this->render('select_fiche/index.html.twig', [
+            'form' => $form->createView(),
+            'controller_name' => 'SelectFicheController',
+        ]);
+
+
+    }
+}
---
+++ src/Form/SelectFicheType.php
@@ -0,0 +1,47 @@
+<?php
+// src/Form/SelectFicheType.php
+
+namespace App\Form;
+
+use App\Entity\FicheFrais;
+use App\Repository\FicheFraisRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class SelectFicheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $user = $options['user'];
+
+        $builder
+            ->add('fiche', EntityType::class, [
+                'class' => FicheFrais::class,
+                'choice_label' => function (FicheFrais $ficheFrais) {
+                    return $ficheFrais->getMois()->format('Y-m'); // Adjust the format as needed
+                },
+                'label' => 'Fiche',
+                'placeholder' => 'Choisir une fiche',
+                'required' => true,
+                'query_builder' => function (FicheFraisRepository $repo) use ($user) {
+                    return $repo->createQueryBuilder('f')
+                        ->where('f.User = :user')
+                        ->setParameter('user', $user);
+                },
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'user' => null,
+        ]);
+    }
+}
